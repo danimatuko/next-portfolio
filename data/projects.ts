e664fb4e6e518462e684108d7793648f7bbd05be
Dani Matuko <matuko305@gmail.com>
2023-04-17 13:29:28 +0300

    add epicstory

--- data/projects.ts
+++ data/projects.ts
@@ -1,33 +1,33 @@
-import { IProject } from '../interface/IProject';
+import { IProject } from "../interface/IProject";
 
 export const projects: IProject[] = [
   {
-    name: 'Topicks',
-    description: 'Blog about anything',
-    src: 'https://res.cloudinary.com/matuko-cloud/image/upload/v1669908394/danimatuko/topicks_dg4jdl.png',
-    demoURL: 'https://topicks-client.vercel.app/',
-    repoURL: 'https://github.com/danimatuko/topicks',
+    name: "Epicstory",
+    description: "This is a PHP blog application powered by MySQL database",
+    src: "https://res.cloudinary.com/matuko-cloud/image/upload/v1681727270/danimatuko/epicstory_cc1grn.png",
+    demoURL: "http://epicstory.epizy.com/",
+    repoURL: "https://github.com/danimatuko/epic-story",
   },
   {
-    name: 'Firetask',
-    description: 'Task mangement system for teams',
-    src: 'https://res.cloudinary.com/matuko-cloud/image/upload/v1669908394/danimatuko/firetask_qmqhmh.png',
-    demoURL: 'https://firetask-danimatuko.web.app/',
-    repoURL: 'https://github.com/danimatuko/firetask',
+    name: "Topicks",
+    description: "Blog about anything",
+    src: "https://res.cloudinary.com/matuko-cloud/image/upload/v1669908394/danimatuko/topicks_dg4jdl.png",
+    demoURL: "https://topicks-client.vercel.app/",
+    repoURL: "https://github.com/danimatuko/topicks",
+  },
+  {
+    name: "Firetask",
+    description: "Task mangement system for teams",
+    src: "https://res.cloudinary.com/matuko-cloud/image/upload/v1669908394/danimatuko/firetask_qmqhmh.png",
+    demoURL: "https://firetask-danimatuko.web.app/",
+    repoURL: "https://github.com/danimatuko/firetask",
   },
 
   {
-    name: 'Gocamp',
-    description: 'eCommerce platform for camping products',
-    src: 'https://res.cloudinary.com/matuko-cloud/image/upload/v1669908394/danimatuko/gocamp_ms9qyg.png',
-    demoURL: 'https://gocamp-client.vercel.app/',
-    repoURL: 'https://github.com/danimatuko/gocamp',
-  },
-  {
-    name: 'edgeLdger',
-    description: 'Agancey landing page',
-    src: 'https://res.cloudinary.com/matuko-cloud/image/upload/v1669908660/danimatuko/edgeLdger_yixxc0.png',
-    demoURL: 'https://eledger.netlify.app/',
-    repoURL: 'https://github.com/DaniMatuko/EdgeLedger',
+    name: "Gocamp",
+    description: "eCommerce platform for camping products",
+    src: "https://res.cloudinary.com/matuko-cloud/image/upload/v1669908394/danimatuko/gocamp_ms9qyg.png",
+    demoURL: "https://gocamp-client.vercel.app/",
+    repoURL: "https://github.com/danimatuko/gocamp",
   },
 ];
